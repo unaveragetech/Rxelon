5cddec9479788832c354b8ba923396c29a8a36e6
jesse winghart <llewknight88@gmail.com>
2025-02-23 14:39:28 -0500

    Create exo_cluster.py

---
+++ exo_cluster.py
@@ -0,0 +1,9 @@
+class EXOCluster:
+    def __init__(self):
+        self.nodes = []
+
+    def add_node(self, node):
+        self.nodes.append(node)
+
+    def remove_node(self, node):
+        self.nodes.remove(node)
